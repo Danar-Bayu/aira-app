ab53d869344a79db4ff1b0cf0feb745b1bb4f024
Danar Bayu <danar.bayu.kusumo@gmail.com>
2025-04-09 17:24:16 +0700

    Updates

--- assets/ProfileDokter-9B7PwJyq.js
+++
@@ -1,1 +0,0 @@
-import{c as t,t as e}from"./index-BxOtkteR.js";var r=e("<h1>Profil");function l(o){const[a,s]=t({name:"",dob:"",sip:"",telp:"",specialty:"",address:""});return r()}export{l as default};
